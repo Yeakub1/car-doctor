d755f2bebdb31ba98045d6cfd4a1e476ea55947c
Yeakub1 <mryeakub10@gmail.com>
2023-05-12 23:23:29 +0600

    booking add

--- src/components/Pages/BookServices/Bookservices.jsx
+++ src/components/Pages/BookServices/Bookservices.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
 import img from "../../../assets/images/checkout/checkout.png";
+import { authContext } from "../../Providers/AuthProviders";
 
 const Bookservices = () => {
   const services = useLoaderData();
-  const { title, price } = services;
+    const { title, price } = services;
+    const { user } = useContext(authContext);
+
+    const handleBookservices = event => {
+        event.preventDefault();
+        const from = event.target;
+        const name = from.name.value;
+        const price = from.price.value;
+        const email = from.email.value;
+        const date = from.date.value;
+        const message = from.message.value;
+        const book = { name, price, email, date, message };
+        console.log(book);
+    }
   return (
     <div>
       <div class="relative overflow-hidden rounded-lg cursor-pointer">
@@ -19,7 +33,7 @@ const Bookservices = () => {
       <h1 className="font-bold text-3xl text-center mt-10 text-orange-500">
         {title}
       </h1>
-      <form>
+      <form onSubmit={handleBookservices}>
         <div className="mt-14 px-10">
           <div className="flex gap-10 ">
             <input
@@ -32,31 +46,32 @@ const Bookservices = () => {
             <input
               className="py-2 w-6/12 bg-slate-200 ps-2 rounded-md"
               type="text"
-              name="name"
+              name="price"
               placeholder="Service Price"
+              defaultValue={"$" + " " + price}
             />
           </div>
           <div className="flex gap-10  mt-8">
             <input
               className="py-2 w-6/12 bg-slate-200 ps-2 rounded-md"
               type="text"
-              name="name"
+              name="email"
               placeholder="Text here"
+              defaultValue={user?.email}
             />{" "}
             <br />
             <input
               className="py-2 w-6/12 bg-slate-200 ps-2 rounded-md"
-              type="text"
-              name="name"
-              defaultValue={price}
+              type="date"
+              name="date"
             />
           </div>
           <textarea
             className="bg-slate-200 mt-8 ps-2 pt-2 rounded-md"
             rows="6"
             cols="147"
-            name="comment"
-            form="usrform"
+            name="message"
+          
             placeholder="Product Description"
           ></textarea>
           <button
